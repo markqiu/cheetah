c15fffbc1afe6d7aa154d6fe2728cd459f2fa4e6
邱承 <qiucheng1@sina.com>
2020-05-14 17:13:20 +0800

    增加测试用例并调通

--- cheetah/super_dataframe.py
+++ cheetah/super_dataframe.py
@@ -27,18 +27,13 @@ def load_metadata(conf_file) -> SuperDataFrameModel:
     with open(conf_file, "r", encoding="utf8") as file:
         conf = yaml.load(file)
         try:
-            data_item_dict = {}
-            for item in conf["DataItems"]:
-                logging.debug(f"正在处理DataItem：{item}")
-                conf["DataItems"][item]["name"] = item
-                data_item_dict[item] = DataItem(**conf["DataItems"][item])
             factor_dict = {}
             for item in conf["Factors"]:
                 logging.debug(f"正在处理Factor：{item}")
                 conf["Factors"][item]["name"] = item
                 factor_dict[item] = Factor(**conf["Factors"][item])
-            sdf = SuperDataFrameModel(data_items=data_item_dict, factors=factor_dict)
-            logging.info(f"处理配置完成，DataItems: {[*conf['DataItems'].keys()]}, Factors: {[*conf['Factors'].keys()]}")
+            sdf = SuperDataFrameModel(columns=factor_dict)
+            logging.info(f"处理配置完成，Factors: {[*conf['Factors'].keys()]}")
         except Exception:
             logging.error(f"读取数据配置发生错误，请检查配置是否合法！错误配置名称: {item}， 信息：{conf}", exc_info=True)
             quit(-1)
@@ -50,7 +45,7 @@ class Column(SuperModel):
     dtype: str
     belong_to: ConstrainedStr  # 从哪张adam表获取
     description: NoneStr
-    alias: Optional[List[ConstrainedStr]]  # 数据列的别名
+    alias: Optional[List]  # 数据列的别名，第一个必须是数据表的列名，及belong_to中设置的数据表的列名。如果没有设置，则列名与name相同。如果有多个别名，则从第二个开始是其他的别名。
 
     class Config:
         arbitrary_types_allowed = [np.ndarray]
@@ -60,27 +55,44 @@ class Index(BaseModel):
     tdate: Column = Field(..., description="数据的日期")
 
 
-class DataItem(Column):
-    """ 数据项是可以从某个数据源获取的数据。数据项可以使因子，但因子不一定是数据。
-    """
-
-    pass
-
-
 class Factor(Column):
     """因子。因子是通过数据计算出来的。
     """
 
     calculate_method: Optional[Tuple[Callable, List]]
-    dependencies: Optional[List[ConstrainedStr]]
+    dependencies: Optional[List]
 
 
 class SuperDataFrameModel(SuperModel):
-    data_items: Dict[ConstrainedStr, DataItem]
-    factors: Dict[ConstrainedStr, Factor]
+    columns: Dict[ConstrainedStr, Factor]
 
     def get_column(self, col_name: str) -> Column:
         return self.columns[col_name]
+
+    def has_column(self, col_name: ConstrainedStr) -> bool:
+        return col_name in self.columns
+
+    def get_belong_to(self, col_name: ConstrainedStr) -> str or None:
+        if self.has_column(col_name):
+            return self.columns[col_name].belong_to
+        else:
+            return None
+
+    def get_table_colname(self, col_name: ConstrainedStr) -> str or None:
+        if self.has_column(col_name):
+            alias = self.get_alias(col_name)
+            if alias:
+                return alias[0]
+            else:
+                return col_name
+        else:
+            return None
+
+    def get_alias(self, col_name: ConstrainedStr) -> str or None:
+        if self.has_column(col_name):
+            if "alias" in self.columns[col_name].fields:
+                return self.columns[col_name].alias
+        return None
 
     def update_column(self, col: Column):
         if col.name in self.columns:
@@ -137,7 +149,7 @@ class SuperDataFrameMan(DataMan):
         :param start: 数据的开始时间，缺省为20100101
         """
         super().__init__("super_data_frame", "超级数据框", start, today_toggle=today_toggle)
-        self.sdfman = self.check_config(load_metadata(config_file_name))
+        self.sdf = self.check_config(load_metadata(config_file_name))
         self.init_dataframe()
 
     def check_config(self, config: SuperDataFrameModel):
@@ -150,7 +162,7 @@ class SuperDataFrameMan(DataMan):
 
     def init_dataframe(self):
         """以交易日为索引，建立起初始数据框"""
-        self.sdf = None
+        pass
 
     def update_data(self):
         pass
--- tests/test_super_dataframe.py
+++ tests/test_super_dataframe.py
@@ -5,5 +5,9 @@ from cheetah.super_dataframe import SuperDataFrameMan, load_metadata
 
 
 def test_load_metadata(conf_dir):
-    logging.basicConfig(level=logging.DEBUG)
-    sdf = SuperDataFrameMan(os.path.join(conf_dir, "metadata.yaml"))
+    sdf_man = SuperDataFrameMan(os.path.join(conf_dir, "metadata.yaml"))
+    assert "CHDQUOTE_ADJ" == sdf_man.sdf.get_belong_to("symbol")
+    assert ["证券代码"] == sdf_man.sdf.get_alias("symbol")
+    assert "证券代码" == sdf_man.sdf.get_table_colname("symbol")
+    assert "risk_signal_identity" == sdf_man.sdf.get_table_colname("risk_signal_identity")
+    assert "TCLOSE_20" == sdf_man.sdf.get_table_colname("20日前收盘价")
